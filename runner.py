43828c55eaff1d1a18098266706e0df0cf86e36e
Andrea Micheli <amicheli@fbk.eu>
2021-12-03 10:57:27 +0100

    Removed debug code

--- runner.py
+++ runner.py
@@ -4,11 +4,6 @@ import sys
 import pytest
 
 TEST_REPO_DIR = os.path.dirname(os.path.realpath(__file__))
-UPF_DIR = os.path.abspath(os.path.join(TEST_REPO_DIR, "../upf")) # Change me if needed
-
-print(UPF_DIR)
-
-sys.path.append(UPF_DIR)
 
 if __name__ == "__main__":
     pytest.main([os.path.join(TEST_REPO_DIR, 'ptc/classical_planning/operation_modes/oneshot')])
