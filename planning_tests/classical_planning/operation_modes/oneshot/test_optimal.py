eaa6ec1a99213047a982b23ba1f5c0865c93e8fe
Gabriele Roeger <gabriele.roeger@unibas.ch>
2022-04-10 21:45:03 +0200

    test optimal classical one-shot planners (also on unsolvable task)

---
+++ planning_tests/classical_planning/operation_modes/oneshot/test_optimal.py
@@ -0,0 +1,65 @@
+import unified_planning as up
+from unified_planning.shortcuts import *
+from unified_planning.solvers.results import SOLVED_OPTIMALLY, UNSOLVABLE_PROVEN, UNSOLVABLE_INCOMPLETELY
+
+from unittest import TestCase, main
+from planning_tests.classical_planning.problems.problem_basic import UPBasic
+from planning_tests.classical_planning.problems.problem_basic_unsolvable import UPBasicUnsolvable
+
+class TestOptimal(TestCase):
+    def setUp(self):
+        TestCase.setUp(self)
+        self.solvable_problem = UPBasic(expected_version=1)
+        self.unsolvable_problem = UPBasicUnsolvable(expected_version=1)
+
+    def _run_solver_and_check_result(self, planner, problem):
+        up_problem = problem.get_problem()
+        result = planner.solve(up_problem)
+        if result.plan:
+            assert result.status is SOLVED_OPTIMALLY
+            assert problem.optimal_cost() == len(result.plan.actions)
+            with PlanValidator(problem_kind=up_problem.kind) as validator:
+                check = validator.validate(up_problem, result.plan)
+                self.assertTrue(check)
+        else:
+            assert result.status in (UNSOLVABLE_PROVEN,
+                                     UNSOLVABLE_INCOMPLETELY)
+            if result.status == UNSOLVABLE_PROVEN:
+                assert problem.optimal_cost() == -1
+                # TODO need constant for unsolvability of task
+                # instead of using -1
+
+    def test_optimal_on_solvable(self):
+        problem = self.solvable_problem
+        up_problem = problem.get_problem()
+        optimal = unified_planning.solvers.solver.SOLVED_OPTIMALLY
+        planner_names = [n for n, s in get_env().factory.solvers.items()
+                        if s.is_oneshot_planner() and
+                        s.satisfies(optimal)]
+
+        print()
+        for p in planner_names:
+            with OneshotPlanner(name=p) as planner:
+                if planner.supports(up_problem.kind):
+                    print("Testing optimal planner", p, "on solvable task")
+                    self._run_solver_and_check_result(planner, problem)
+
+    def test_optimal_on_unsolvable(self):
+        problem = self.unsolvable_problem
+        up_problem = problem.get_problem()
+        optimal = unified_planning.solvers.solver.SOLVED_OPTIMALLY
+        planner_names = [n for n, s in get_env().factory.solvers.items()
+                        if s.is_oneshot_planner() and
+                        s.satisfies(optimal)]
+        up_problem = self.unsolvable_problem.get_problem()
+
+        print()
+        for p in planner_names:
+            with OneshotPlanner(name=p) as planner:
+                if planner.supports(up_problem.kind):
+                    print("Testing optimal planner", p, "on unsolvable task")
+                    self._run_solver_and_check_result(planner, problem)
+
+
+if __name__ == "__main__":
+    main()
