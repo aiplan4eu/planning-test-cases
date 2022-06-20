62ee7265ab44dd903c167967ff99c5d986368883
AfkValerik <v.borelli001@unibs.studenti.it>
2022-06-20 15:37:24 +0200

    fixed planner_names method

--- planning_tests/utility/planner_names.py
+++ planning_tests/utility/planner_names.py
@@ -4,7 +4,7 @@ from unified_planning.shortcuts import*
 
 
 def get_planner_names(problem_kind):
-
-    #return [n for n, s in get_env().factory.engines.items() if s.is_oneshot_planner() and
-    #s.supports(problem_kind)]
-    return ["enhsp"]
+    
+    return [n for n, s in get_env().factory.engines.items() if s.is_oneshot_planner() and
+    s.supports(problem_kind) and n in ["lpg","enhsp","pyperplan","tamer"]]
+    #return ["lpg","enhsp","tamer"]
