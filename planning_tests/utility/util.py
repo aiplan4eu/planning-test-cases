1b8b76fc1dd04814cc45c69a4e30d82a662920e3
AfkValerik <v.borelli001@unibs.studenti.it>
2022-07-04 16:10:38 +0200

    removed unused files

--- planning_tests/utility/util.py
+++ planning_tests/utility/util.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from unified_planning.shortcuts import OneshotPlanner, PlanValidator
-#from planning_tests.utility.correct_output import correct, correct_validation
+
 
 class TestUtil:
 
@@ -9,15 +9,10 @@ class TestUtil:
 	def execute_one_shot_planning_test(problem,planner_name,problem_name = "",expected_plan_length=None ):
 			
 		with OneshotPlanner(name=planner_name) as planner:
+			#this if might be deleted
 			if planner.supports(problem.kind):
 				result = planner.solve(problem)
 				plan = result.plan
-				#if len(sys.argv) > 2 :
-						#if sys.argv[2] == '-v' or sys.argv[2] == '-verbose':
-							#open(f"output","w").write('\n'.join([str(act) for act in plan.plan.actions]))
-							#correct("output")
-							#os.system("./validate  "+ path +"block_grouping_domain.pddl "+ path + name +" output")
-							#print(correct_validation(str(plan)))
 				with PlanValidator(problem_kind=problem.kind,plan_kind=plan.kind) as validator:
 					check = validator.validate(problem, plan)
 					if expected_plan_length is not None:
