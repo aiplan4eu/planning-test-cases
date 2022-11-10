799b41f436eab39a20f0779346ed94078a9fccc6
AfkValerik <v.borelli001@unibs.studenti.it>
2022-11-10 16:36:03 +0100

    aligned with the changes made in UP

--- planning_tests/utility/util.py
+++ planning_tests/utility/util.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from unified_planning.shortcuts import OneshotPlanner, PlanValidator
+from unified_planning.shortcuts import OneshotPlanner, PlanValidator#, AnytimePlanner
 
 
 plans = []
@@ -39,20 +39,20 @@ class TestUtil:
 
 
 
-    @staticmethod
-    def execute_anytime_planning_test(problem,planner_name,problem_name = "",expected_plan_length=None ):
+  #  @staticmethod
+  #  def execute_anytime_planning_test(problem,planner_name,problem_name = "",expected_plan_length=None ):
                     
-        with OneshotPlanner(name=planner_name) as planner:
+  #      with AnytimePlanner(name=planner_name) as planner:
             #this if might be deleted
-            if planner.supports(problem.kind):
-                result = planner.solve(problem,callback = my_callback, timeout = 30)
-                plan = result.plan
-                with PlanValidator(problem_kind=problem.kind,plan_kind=plan.kind) as validator:
-                    check = validator.validate(problem, plan)
-                    if expected_plan_length is not None:
-                        check = check and len(plan.actions) == expected_plan_length
-                    saveArray(plans)
-                    assert check
+  #          if planner.supports(problem.kind):
+  #              result = planner.solve(problem,callback = my_callback, timeout = 30)
+  #              plan = result.plan
+  #              with PlanValidator(problem_kind=problem.kind,plan_kind=plan.kind) as validator:
+  #                  check = validator.validate(problem, plan)
+  #                  if expected_plan_length is not None:
+  #                      check = check and len(plan.actions) == expected_plan_length
+  #                  #saveArray(plans)
+  #                  assert check
 
 
 
