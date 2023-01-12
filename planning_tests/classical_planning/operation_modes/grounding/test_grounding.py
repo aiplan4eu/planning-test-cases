3381413cba99b4860f995307a66a79248224bb1d
Gabriele Roeger <gabriele.roeger@unibas.ch>
2023-01-12 17:17:49 +0100

    more progress on grounding tests

--- planning_tests/classical_planning/operation_modes/grounding/test_grounding.py
+++ planning_tests/classical_planning/operation_modes/grounding/test_grounding.py
@@ -28,9 +28,9 @@ def result_cache():
         def result(self, grounder_name, problem):
             if (grounder_name, problem) not in self._results:
                 up_problem = problem.get_problem()
-                with Compiler(name=oneshot_planner_name) as grounder:
-                    assert(grounder.supports_compilation(CompilationKind.GROUNDING)
-                    res = grouinder.compile(up_problem, CompilationKind.GROUNDING)
+                with Compiler(name=grounder_name) as grounder:
+                    assert(grounder.supports_compilation(CompilationKind.GROUNDING))
+                    res = grounder.compile(up_problem, CompilationKind.GROUNDING)
                     self._results[(grounder_name, problem)] = res
             return self._results[(grounder_name, problem)]
 
@@ -52,29 +52,29 @@ tpp_pfile6 = tpp_pfile6(expected_version=1)
 
 @pytest.mark.all
 @pytest.mark.solvable
-@pytest.mark.parametrize("problem_name, problem, optimal_cost",
+@pytest.mark.parametrize("problem_name, problem",
     [
-        pytest.param('prob_basic', basic, 1, marks=pytest.mark.simple),
-        pytest.param('prob_cond_effects', conditional_effects, 2, marks=pytest.mark.simple),
-        pytest.param('prob_cost_metric', cost_metric, 4,
+        pytest.param('prob_basic', basic, marks=pytest.mark.simple),
+        pytest.param('prob_cond_effects', conditional_effects, marks=pytest.mark.simple),
+        pytest.param('prob_cost_metric', cost_metric,
             marks=pytest.mark.simple),
-        pytest.param('prob_length_metric', length_metric, 1,
+        pytest.param('prob_length_metric', length_metric, 
             marks=pytest.mark.simple),
-        pytest.param('prob_cond_effects', conditional_effects, 2,
+        pytest.param('prob_cond_effects', conditional_effects,
             marks=pytest.mark.simple),
-        pytest.param("depots_pfile1", depots_pfile1, 10,
+        pytest.param("depots_pfile1", depots_pfile1,
             marks=[pytest.mark.simple, pytest.mark.depots]),
-        pytest.param("depots_pfile2", depots_pfile2, 15,
+        pytest.param("depots_pfile2", depots_pfile2,
             marks=[pytest.mark.simple, pytest.mark.depots]),
-        pytest.param("depots_pfile3", depots_pfile3, 27,
+        pytest.param("depots_pfile3", depots_pfile3,
             marks=[pytest.mark.medium, pytest.mark.depots]),
-        pytest.param("tpp_pfile1", tpp_pfile1, 5,
+        pytest.param("tpp_pfile1", tpp_pfile1,
             marks=[pytest.mark.simple, pytest.mark.TPP]),
-        pytest.param("tpp_pfile2", tpp_pfile2, 8,
+        pytest.param("tpp_pfile2", tpp_pfile2,
             marks=[pytest.mark.simple, pytest.mark.TPP]),
-        pytest.param("tpp_pfile3", tpp_pfile3, 11,
+        pytest.param("tpp_pfile3", tpp_pfile3,
             marks=[pytest.mark.simple, pytest.mark.TPP]),
-        pytest.param("tpp_pfile6", tpp_pfile6, 25,
+        pytest.param("tpp_pfile6", tpp_pfile6,
             marks=[pytest.mark.medium, pytest.mark.TPP]),
     ])
 class TestGrounding:
@@ -90,18 +90,13 @@ class TestGrounding:
         ground_problem = result.problem
 
         # solve ground_problem with arbitrary planner
+        with OneshotPlanner(problem_kind=ground_problem.kind) as planner:
+            result = planner.solve(ground_problem)
 
-
-        up_problem = problem.get_problem()
-
-        planner = OneshotPlanner(name=oneshot_planner_name)
-        if not planner.supports(up_problem.kind):
-            pytest.skip("{} does not support problem kind".format(oneshot_planner_name))
-        if not is_simple and planner.satisfies(OptimalityGuarantee.SOLVED_OPTIMALLY):
-            pytest.skip("skip optimal planner on non-simple task")
-        result = result_cache.result(oneshot_planner_name, problem)
         if not result.plan:
-            pytest.skip("{} did not find a plan".format(oneshot_planner_name))
+            pytest.skip("{} did not find a plan".format(planner))
+        transferred_plan = result.plan.replace_action_instances(map_back)
+
         # The plan validator does not support metrics, so we remove this part
         # from the instance
         # TODO remove workaraound once resolved in UP
@@ -110,23 +105,23 @@ class TestGrounding:
         if metrics:
             validation_problem = validation_problem.clone()
             validation_problem.clear_quality_metrics()
-       
         with PlanValidator(problem_kind=validation_problem.kind) as validator:
-            check = validator.validate(validation_problem, result.plan)
+            check = validator.validate(validation_problem, transferred_plan)
             assert check.status is ValidationResultStatus.VALID
 
-    def test_map_back_action_instance_is_complete(self, grounder_name,
-            problem_name, problem, result_cache):
-        up_problem = problem.get_problem()
-        grounder = Compiler(name=grounder_name)
-        if not grounder.supports(up_problem.kind):
-            pytest.skip("{} does not support problem kind".format(grounder_name))
-        result = result_cache.result(grounder_name, problem)
-        map_back = result.map_back_action_instance
-        ground_problem = result.problem
-        for a in ground_problem.actions:
-            assert a in map_back_action_instance
-
+#    def test_map_back_action_instance_is_complete(self, grounder_name,
+#            problem_name, problem, result_cache):
+#        up_problem = problem.get_problem()
+#        grounder = Compiler(name=grounder_name)
+#        if not grounder.supports(up_problem.kind):
+#            pytest.skip("{} does not support problem kind".format(grounder_name))
+#        result = result_cache.result(grounder_name, problem)
+#        map_back = result.map_back_action_instance
+#        ground_problem = result.problem
+#        for a in ground_problem.actions:
+#            la = map_back(a)
+##            assert a in map_back
+#
 
     def test_result_is_grounded(self, grounder_name, problem_name,
             problem, result_cache):
@@ -142,5 +137,3 @@ class TestGrounding:
 
         assert 'UNIVERSAL_CONDITIONS' not in ground_problem.kind.features
         assert 'EXISTENTIAL_CONDITIONS' not in ground_problem.kind.features
-
-        
--- planning_tests/conftest.py
+++ planning_tests/conftest.py
@@ -3,6 +3,10 @@ from unified_planning.engines.mixins.compiler import CompilationKind
 
 import pytest
 
+# TODO remove once in UPF
+env = get_env()
+env.factory.add_engine('fast-downward-grounder', "up_fast_downward", 'FastDownwardGrounder')
+
 def pytest_addoption(parser):
     parser.addoption("--planner_name", action="append", default=[], 
     help="add tests for this planner. Available values: a planner name or 'all'")
@@ -18,7 +22,7 @@ def pytest_generate_tests(metafunc):
         tags = [t for t in tags if get_env().factory.engine(t).is_oneshot_planner()]
         metafunc.parametrize("oneshot_planner_name", tags, scope='session')
     if "grounder_name" in metafunc.fixturenames:
-        tags = set(metafunc.config.option.planner_name)
+        tags = set(metafunc.config.option.grounder_name)
         if not tags or 'all' in tags:
             tags = [n for n in get_env().factory.engines]
         tags = [t for t in tags if
