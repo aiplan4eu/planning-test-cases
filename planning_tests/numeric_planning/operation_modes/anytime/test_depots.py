bd982ccbdcd60a7cb40c3b5fe3947fcf5cadfcad
AfkValerik <v.borelli001@unibs.studenti.it>
2023-02-07 14:07:47 +0100

    added tests for the depots domain

---
+++ planning_tests/numeric_planning/operation_modes/anytime/test_depots.py
@@ -0,0 +1,47 @@
+import unified_planning
+from unified_planning.shortcuts import*
+from planning_tests.utility.util import TestUtil
+from planning_tests.utility.planner_names import get_anytime_names
+from planning_tests.numeric_planning.pddl_problems.depots.depots import depots_pfile1, depots_pfile2, depots_pfile3, depots_pfile10, depots_pfile11
+import pytest
+
+
+
+class TestDepots:
+
+    depots_pfile1 = depots_pfile1(expected_version=1)
+    depots_pfile2 = depots_pfile2(expected_version=1)
+    depots_pfile3 = depots_pfile3(expected_version=1)
+    depots_pfile10 = depots_pfile10(expected_version=1)
+    depots_pfile11 = depots_pfile11(expected_version=1)
+
+    #we check only the first problem, since the domain is the same for all the problems
+    planner_names = get_anytime_names(depots_pfile1.get_problem().kind)
+  
+
+    @pytest.mark.all
+    @pytest.mark.simple 
+    @pytest.mark.depots
+    @pytest.mark.parametrize("planner_name",planner_names)
+    @pytest.mark.parametrize("problem_name,problem",[("depots_pfile1",depots_pfile1 ),
+    ("depots_pfile2",depots_pfile2 ),
+    ("depots_pfile3",depots_pfile3 )])
+    def test_depots(self,planner_name,problem_name,problem):
+        problem_with_metric = problem.get_problem()
+        problem_with_metric.add_quality_metric(MinimizeSequentialPlanLength())
+        TestUtil.execute_anytime_planning_test(problem_with_metric,planner_name,[problem_name +'.pddl'])
+
+
+    @pytest.mark.all
+    @pytest.mark.medium
+    @pytest.mark.depots
+    @pytest.mark.parametrize("planner_name",planner_names)
+    @pytest.mark.parametrize("problem_name,problem",[("depots_pfile10",depots_pfile10 ),
+    ("depots_pfile11",depots_pfile11 )])
+    def test_depots_medium(self,planner_name,problem_name,problem):
+        problem_with_metric = problem.get_problem()
+        problem_with_metric.add_quality_metric(MinimizeSequentialPlanLength())
+        TestUtil.execute_anytime_planning_test(problem_with_metric,planner_name,[problem_name +'.pddl'])
+
+ 
+
--- planning_tests/utility/planner_names.py
+++ planning_tests/utility/planner_names.py
@@ -5,15 +5,15 @@ from unified_planning.shortcuts import*
 
 def get_planner_names(problem_kind):
     return [n for n, s in get_env().factory._engines.items() if s.is_oneshot_planner() and 
-    s.supports(problem_kind) and n in ["lpg","enhsp","pyperplan","tamer","fast-downward"]]
+    s.supports(problem_kind) and n in ["lpg","enhsp"]]
     #["lpg","enhsp","pyperplan","tamer"]
 
 def get_planner_names_opt(problem_kind):
         return [n for n, s in get_env().factory._engines.items() if s.is_oneshot_planner() and 
         s.supports(problem_kind) and n in ["enhsp-opt"]]
 
-def anytime_names(problem_kind):
-    return [n for n, s in get_env().factory.engines if s.is_oneshot_planner() and
+def get_anytime_names(problem_kind):
+    return [n for n, s in get_env().factory._engines.items() if s.is_oneshot_planner() and 
     s.supports(problem_kind) and n in ["lpg-anytime"]]
 
 
