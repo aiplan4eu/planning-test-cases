6ec4d2b5f75280c04fc2d56359c95912e9abb2c5
Roland Godet <r.godet@outlook.fr>
2023-08-31 13:44:34 +0200

    chore(sched): parse jobshop plans

--- planning_tests/scheduling_planning/jobshop/__init__.py
+++ planning_tests/scheduling_planning/jobshop/__init__.py
@@ -18,29 +18,31 @@ def _parse_line(line: str) -> list[int]:
     return list(map(int, line.rstrip().split()))
 
 
-def _parse_problem(filepath: Path) -> SchedulingProblem:
+# pylint: disable = too-many-locals
+def _parse(filepath: Path) -> tuple[SchedulingProblem, SchedulingPlan, int]:
     """
-    Parses the JobShop instance stored in the given file.
+    Parses the JobShop problem instance stored in the given file.
 
     # Content example
 
     ```
-    nb_jobs nb_machines
-    6 6 0 0 0 0
+    nb_jobs nb_machines optimal
+    4 4 37 0
     Times
-    1 3 6 7 3 6
-    8 5 10 10 10 4
-    5 4 8 9 1 7
-    5 5 5 3 8 9
-    9 3 5 4 3 1
-    3 3 9 10 4 1
+    5 4 8 9
+    5 5 5 3
+    9 3 5 4
+    3 3 9 10
     Machines
-    3 1 2 4 6 5
-    2 3 5 6 1 4
-    3 4 6 1 2 5
-    2 1 3 4 5 6
-    3 2 5 6 1 4
-    2 4 6 1 5 3
+    3 4 1 2
+    2 1 3 4
+    3 2 1 4
+    2 4 1 3
+    Starts
+    0 6 20 28
+    3 15 25 30
+    5 14 28 33
+    0 3 6 15
     ```
     """
 
@@ -50,7 +52,7 @@ def _parse_problem(filepath: Path) -> SchedulingProblem:
 
     # Get header data
     lines.pop(0)  # Remove the header
-    num_jobs, num_machines = _parse_line(lines.pop(0))[0:2]
+    num_jobs, num_machines, optimal = _parse_line(lines.pop(0))[0:3]
 
     # Get times
     lines.pop(0)  # Remove the header
@@ -64,12 +66,19 @@ def _parse_problem(filepath: Path) -> SchedulingProblem:
     for _ in range(num_jobs):
         machines.append(_parse_line(lines.pop(0))[0:num_machines])
 
-    # Create the problem
+    # Get starts
+    lines.pop(0)  # Remove the header
+    starts = []
+    for _ in range(num_jobs):
+        starts.append(_parse_line(lines.pop(0))[0:num_machines])
+
+    # Create the problem and the plan data
     problem = SchedulingProblem(f"sched:jobshop:{filepath.name}")
     machine_objects = [
         problem.add_resource(f"M{i}", capacity=1) for i in range(1, num_machines + 1)
     ]
 
+    timepoints = {}
     for job in range(num_jobs):
         previous_activity = None
         for task in range(num_machines):
@@ -81,6 +90,11 @@ def _parse_problem(filepath: Path) -> SchedulingProblem:
             if previous_activity is not None:
                 problem.add_constraint(LE(previous_activity.end, activity.start))
             previous_activity = activity
+            timepoints[activity.start] = starts[job][task]
+            timepoints[activity.end] = starts[job][task] + times[job][task]
     problem.add_quality_metric(MinimizeMakespan())
-    print(problem)
-    return problem
+
+    # Create the plan
+    plan = SchedulingPlan(problem.activities, timepoints)
+
+    return problem, plan, optimal
