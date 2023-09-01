37fc89e34c055a51890118b50a60de6b624519f6
Roland Godet <r.godet@outlook.fr>
2023-09-01 11:10:38 +0200

    refactor(sched): regroup all plans in the problem file

--- planning_tests/scheduling_planning/jobshop/__init__.py
+++ planning_tests/scheduling_planning/jobshop/__init__.py
@@ -17,42 +17,7 @@ def problems() -> list[TestCase]:
     """
     Generates deterministically a set of JobShop problems with valid/invalid solutions and optimal.
     """
-    base_dir = Path(__file__).parent.resolve()
-    valid_problems = _problems_from_folder(base_dir / "valid_problems")
-    invalid_problems = _problems_from_folder(base_dir / "invalid_problems")
-
-    pbs_map = {}
-    for problem, plan, optimal in valid_problems:
-        if problem not in pbs_map:
-            pbs_map[problem] = {
-                "optimum": optimal,
-                "valid_plans": [],
-                "invalid_plans": [],
-            }
-        pbs_map[problem]["valid_plans"].append(plan)
-
-    for problem, plan, optimal in invalid_problems:
-        if problem not in pbs_map:
-            pbs_map[problem] = {
-                "optimum": optimal,
-                "valid_plans": [],
-                "invalid_plans": [],
-            }
-        pbs_map[problem]["invalid_plans"].append(plan)
-
-    return [
-        TestCase(
-            problem,
-            solvable=True,
-            **data,
-        )
-        for problem, data in pbs_map.items()
-    ]
-
-
-def _problems_from_folder(
-    folder: Path,
-) -> list[tuple[SchedulingProblem, SchedulingPlan, int]]:
+    folder = Path(__file__).parent.resolve() / "problems"
     return [_parse(file) for file in folder.iterdir()]
 
 
@@ -75,9 +40,9 @@ def _parse_line(line: str) -> list[int]:
 
 
 # pylint: disable = too-many-locals
-def _parse(filepath: Path) -> tuple[SchedulingProblem, SchedulingPlan, int]:
+def _parse(filepath: Path) -> TestCase:
     """
-    Parses the JobShop problem instance stored in the given file.
+    Parses the JobShop test case instance stored in the given file.
 
     # Content example
 
@@ -94,11 +59,16 @@ def _parse(filepath: Path) -> tuple[SchedulingProblem, SchedulingPlan, int]:
     2 1 3 4
     3 2 1 4
     2 4 1 3
-    Starts
+    Valid Plan
     0 6 20 28
     3 15 25 30
     5 14 28 33
     0 3 6 15
+    Invalid Plan
+    0 1 11 17
+    3 11 16 26
+    0 3 6 16
+    1 17 26 36
     ```
     """
 
@@ -108,50 +78,66 @@ def _parse(filepath: Path) -> tuple[SchedulingProblem, SchedulingPlan, int]:
 
     # Get header data
     lines.pop(0)  # Remove the header
-    num_jobs, num_machines, optimal = _parse_line(lines.pop(0))[0:3]
+    nb_jobs, nb_machines, optimal = _parse_line(lines.pop(0))[0:3]
 
     # Get times
     lines.pop(0)  # Remove the header
-    times = []
-    for _ in range(num_jobs):
-        times.append(_parse_line(lines.pop(0))[0:num_machines])
+    times = [_parse_line(lines.pop(0))[0:nb_machines] for _ in range(nb_jobs)]
 
     # Get machines
     lines.pop(0)  # Remove the header
-    machines = []
-    for _ in range(num_jobs):
-        machines.append(_parse_line(lines.pop(0))[0:num_machines])
+    machines = [_parse_line(lines.pop(0))[0:nb_machines] for _ in range(nb_jobs)]
 
     # Get starts
-    lines.pop(0)  # Remove the header
-    starts = []
-    for _ in range(num_jobs):
-        starts.append(_parse_line(lines.pop(0))[0:num_machines])
+    valid_starts, invalid_starts = [], []
+    while len(lines) > 0 and "plan" in (header := lines.pop(0).lower()):
+        starts = [_parse_line(lines.pop(0))[0:nb_machines] for _ in range(nb_jobs)]
+        if "invalid" in header:
+            invalid_starts.append(starts)
+        elif "valid" in header:
+            valid_starts.append(starts)
+        else:
+            raise ValueError(f"Cannot parse section with header '{header}'")
 
-    # Create the problem and the plan data
-    problem_id = filepath.name.split("_")[2].replace(".jsp", "")
-    problem = SchedulingProblem(f"sched:jobshop:problem_{problem_id}")
+    # Create the problem
+    problem = SchedulingProblem(f"sched:jobshop:{filepath.stem}")
+    activities = []
     machine_objects = [
-        problem.add_resource(f"M{i}", capacity=1) for i in range(1, num_machines + 1)
+        problem.add_resource(f"M{i+1}", capacity=1) for i in range(nb_machines)
     ]
 
-    timepoints = {}
-    for job in range(num_jobs):
+    for job in range(nb_jobs):
         previous_activity = None
-        for task in range(num_machines):
+        for task in range(nb_machines):
             activity = problem.add_activity(
-                f"T_{job+1}_{task+1}", duration=times[job][task]
+                f"T_{job+1}_{task+1}",
+                duration=times[job][task],
             )
             machine = machine_objects[machines[job][task] - 1]
             activity.uses(machine)
             if previous_activity is not None:
                 problem.add_constraint(LE(previous_activity.end, activity.start))
             previous_activity = activity
-            timepoints[activity.start] = starts[job][task]
-            timepoints[activity.end] = starts[job][task] + times[job][task]
+            activities.append(activity)
     problem.add_quality_metric(MinimizeMakespan())
 
-    # Create the plan
-    plan = SchedulingPlan(problem.activities, timepoints)
+    # Create the plans
+    valid_plans, invalid_plans = [], []
+    for starts in valid_starts + invalid_starts:
+        timepoints = {}
+        k = 0
+        for job in range(nb_jobs):
+            for task in range(nb_machines):
+                start = starts[job][task]
+                end = start + times[job][task]
+                activity = activities[k]
+                timepoints[activity.start] = start
+                timepoints[activity.end] = end
+                k += 1
+        plan = SchedulingPlan(activities, timepoints)
+        if starts in valid_starts:
+            valid_plans.append(plan)
+        else:
+            invalid_plans.append(plan)
 
-    return problem, plan, optimal
+    return TestCase(problem, True, optimal, valid_plans, invalid_plans)
